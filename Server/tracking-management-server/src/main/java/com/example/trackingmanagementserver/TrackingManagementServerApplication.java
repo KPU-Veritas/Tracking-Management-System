532bdd20fdeacbcea16915cd1ad00f2e2b66e0bc
JungHoiMin <wony1102@naver.com>
2022-01-15 16:04:02 +0900

    Web&Server Project Created

---
+++ Server/tracking-management-server/src/main/java/com/example/trackingmanagementserver/TrackingManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.trackingmanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingManagementServerApplication.class, args);
+	}
+
+}
