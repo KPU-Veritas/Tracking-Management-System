65346aa15af5423f440c03e9583f8d8965cb9f0a
JungHoiMin <wony1102@naver.com>
2022-01-17 00:00:10 +0900

    created about user

---
+++ Server/tracking-management-server/src/main/java/com/example/trackingmanagementserver/config/WebSecurityConfig.java
@@ -0,0 +1,6 @@
+// P.267 ¿€º∫
+package com.example.trackingmanagementserver.config;
+
+public class WebSecurityConfig {
+
+}
---
+++ Server/tracking-management-server/src/main/java/com/example/trackingmanagementserver/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.example.trackingmanagementserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+	private String token;
+	private String uuid;
+	private String username;
+	private String email;
+	private String password;
+	private String phoneNumber;
+	private String simpleAddress;
+	private String detailAddress;
+}
