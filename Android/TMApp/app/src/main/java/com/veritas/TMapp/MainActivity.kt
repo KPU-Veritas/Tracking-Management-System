9bf3629ff65cc0bb4a9d437847232909d59b2465
hklee4578 <hklee4578@naver.com>
2022-02-15 22:15:21 +0900

    Android SDK

--- Android/TMApp/app/src/main/java/com/veritas/TMapp/MainActivity.kt
+++ Android/TMApp/app/src/main/java/com/veritas/TMapp/MainActivity.kt
@@ -165,7 +165,7 @@ class MainActivity : AppCompatActivity() {
         }
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R) {
             // Uncomment line below if targeting SDK 31
-            // permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH_SCAN)
+            permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH_SCAN)
             permissionRationale ="This app needs both fine location permission and nearby devices permission to detect beacons.  Please grant both now."
         }
         var allGranted = true
