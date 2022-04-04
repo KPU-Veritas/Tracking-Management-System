fcd21c3cbf419be550ac108378d6c3591352fa39
Beomjun Kim <malin0523@naver.com>
2022-04-04 23:50:43 +0900

    다음 주소 수정 중 (해결 실패)

--- Android/TMApp/app/src/main/java/com/veritas/TMapp/sign/FindAddressActivity.kt
+++ Android/TMApp/app/src/main/java/com/veritas/TMapp/sign/FindAddressActivity.kt
@@ -2,16 +2,18 @@ package com.veritas.TMapp.sign
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
+import android.widget.Button
 import com.veritas.TMapp.R
 
 class FindAddressActivity : AppCompatActivity() {
     private var browser: WebView? = null
+    private var textViewSimpleAddress: TextView? = null
 
     internal inner class MyJavaScriptInterface {
         @JavascriptInterface
@@ -29,6 +31,8 @@ class FindAddressActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_address)
         browser = findViewById<View>(R.id.webView) as WebView
+        textViewSimpleAddress = findViewById<View>(R.id.textViewSimpleAddress) as TextView
+        val buttonSearchAddress = findViewById<View>(R.id.buttonSearchAddress) as Button
         browser!!.settings.javaScriptEnabled = true
         browser!!.addJavascriptInterface(MyJavaScriptInterface(), "Android")
         browser!!.webViewClient = object : WebViewClient() {
@@ -38,5 +42,19 @@ class FindAddressActivity : AppCompatActivity() {
         }
 
         browser!!.loadUrl("http://www.inspond.com/daum.html")
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        when (requestCode) {
+            SEARCH_ADDRESS_ACTIVITY -> if (resultCode == RESULT_OK) {
+                val data = intent!!.extras?.getString("data")
+                if (data != null) browser!!.text = data
+            }
+        }
+    }
+
+    companion object {
+        private const val SEARCH_ADDRESS_ACTIVITY = 10000
     }
 }
