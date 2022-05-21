1b82e97b1eb4d97e800aa79bd55f1eee515c69cd
zennoryun <hklee4578@naver.com>
2022-05-22 02:34:43 +0900

    비콘 라이브러리 버전 변경, 다이얼로그 한글화

--- Android/TMApp/app/src/main/java/com/veritas/TMapp/MainActivity.kt
+++ Android/TMApp/app/src/main/java/com/veritas/TMapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.veritas.TMapp
 
 import android.Manifest
+import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -61,12 +62,6 @@ class MainActivity : AppCompatActivity() {
         dbController = DBController()
         beaconScannerApplication = application as BeaconScannerApplication
         saveFcmToken()
-        // Set up a Live Data observer for beacon data
-        val regionViewModel = BeaconManager.getInstanceForApplication(this).getRegionViewModel(beaconScannerApplication.region)
-        // observer will be called each time the monitored regionState changes (inside vs. outside region)
-        regionViewModel.regionState.observe(this, monitoringObserver)
-        // observer will be called each time a new list of beacons is ranged (typically ~1 second in the foreground)
-        regionViewModel.rangedBeacons.observe(this, rangingObserver)
         val beaconManager = BeaconManager.getInstanceForApplication(this)
         // 뷰페이저의 페이지뷰를 생성하기 위해 사용되는 어댑터 클래스
         val adapter = PagerAdapter(supportFragmentManager)
@@ -87,32 +82,6 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         checkPermissions()
     }
-    // 비콘을 모니터링하고 모니터링 결과를 로그로 출력
-    // 비콘 감지 다이얼로그 주석 처리
-    private val monitoringObserver = Observer<Int> { state ->
-        //var dialogTitle = "Beacons detected"
-        //var dialogMessage = "didEnterRegionEvent has fired"
-        var stateString = "inside"
-        if (state == MonitorNotifier.OUTSIDE) {
-            /*dialogTitle = "No beacons detected"
-            dialogMessage = "didExitRegionEvent has fired"*/
-            stateString = "outside"
-        }
-        Log.d(TAG, "monitoring state changed to : $stateString")
-
-        //showDialogs(dialogTitle,dialogMessage,null)
-    }
-
-    private val rangingObserver = Observer<Collection<Beacon>> { beacons ->
-        Log.d(TAG, "감지된 기기 수: ${beacons.count()}개")
-        if (BeaconManager.getInstanceForApplication(this).rangedRegions.isNotEmpty()) {
-            //binding.beaconCount.text = "Ranging enabled: ${beacons.count()} beacon(s) detected"
-            /*binding.beaconList.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
-                beacons
-                    .sortedBy { it.distance }
-                    .map { "${it.id1}\nid2: ${it.id2} id3: ${it.id3}  rssi: ${it.rssi}\nest. distance: ${it.distance} m" }.toTypedArray())*/
-        }
-    }
     // 권한 요청 결과를 가져옴
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -128,10 +97,10 @@ class MainActivity : AppCompatActivity() {
     private fun checkPermissions() {
         // basepermissions are for M and higher
         var permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-        //var permissionRationale ="This app needs both fine location permission and background location permission to detect beacons in the background.  Please grant both now."
+        var permissionRationale ="이 앱은 백그라운드에서 비콘을 탐지하려면 정확한 위치 권한과 백그라운드 위치 권한이 모두 필요합니다. 지금 권한들을 허용해 주십시오."
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
             permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION)
-            //permissionRationale ="This app needs fine location permission and nearby devices permission to detect beacons.  Please grant this now."
+            permissionRationale ="이 앱은 비콘을 탐지하기 위해 정확한 위치 권한이 필요합니다. 지금 위치 권한을 허용해 주십시오."
         }
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -143,7 +112,7 @@ class MainActivity : AppCompatActivity() {
                     Manifest.permission.BLUETOOTH_CONNECT
                 )
             }
-            //permissionRationale ="This app needs both fine location permission and nearby devices permission to detect beacons.  Please grant both now."
+            permissionRationale ="이 앱은 비콘을 탐지하기 위해 정확한 위치 권한과 블루투스 권한이 모두 필요합니다. 지금 권한들을 허용해 주십시오."
         }
         var allGranted = true
         for (permission in permissions) {
@@ -151,26 +120,22 @@ class MainActivity : AppCompatActivity() {
         }
         if (!allGranted) {
             if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-                /*showDialogs(
-                    "This app needs permissions to detect beacons",
+                showDialogs(
+                    "이 앱은 비콘을 탐지할 수 있는 권한이 필요합니다.",
                     permissionRationale,
                     requestPermissions(
                         permissions,
                         PERMISSION_REQUEST_FINE_LOCATION
                     )
-                )*/
-                requestPermissions(
-                    permissions,
-                    PERMISSION_REQUEST_FINE_LOCATION
                 )
             }
-            /*else {
+            else {
                 showDialogs(
-                    "Functionality limited",
-                    "Since location and device permissions have not been granted, this app will not be able to discover beacons.  Please go to Settings -> Applications -> Permissions and grant location and device discovery permissions to this app.",
+                    "기능이 제한됨",
+                    "위치 및 블루투스 권한이 부여되지 않았기 때문에 이 앱은 비콘을 검색할 수 없습니다. 설정 -> 응용프로그램 -> 사용 권한으로 이동하여 이 앱에 위치 및 블루투스 권한을 허용하십시오.",
                     null
                 )
-            }*/
+            }
         }
         else {
             if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
@@ -178,30 +143,26 @@ class MainActivity : AppCompatActivity() {
                     != PackageManager.PERMISSION_GRANTED
                 ) {
                     if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
-                        /*showDialogs(
-                            "This app needs background location access",
-                            "Please grant location access so this app can detect beacons in the background.",
+                        showDialogs(
+                            "이 앱은 백그라운드 위치 액세스가 필요합니다.",
+                            "이 앱이 백그라운드에서 비콘을 탐지할 수 있도록 위치 액세스를 부여하십시오.",
                             requestPermissions(
                                 arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
                                 PERMISSION_REQUEST_BACKGROUND_LOCATION
                             )
-                        )*/
-                        requestPermissions(
-                            arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
-                            PERMISSION_REQUEST_BACKGROUND_LOCATION
                         )
-                    } /*else {
+                    } else {
                         showDialogs(
-                            "Functionality limited",
-                            "Since background location access has not been granted, this app will not be able to discover beacons in the background.  Please go to Settings -> Applications -> Permissions and grant background location access to this app.",
+                            "기능이 제한됨",
+                            "백그라운드 위치 액세스가 허용되지 않았기 때문에 이 앱은 백그라운드에서 비콘을 검색할 수 없습니다. 설정 -> 응용프로그램 -> 사용 권한으로 이동하여 이 앱에 대한 백그라운드 위치 액세스를 부여하십시오.",
                             null
                         )
-                    }*/
+                    }
                 }
             }
         }
     }
-    /* 다이얼로그 출력 함수 주석 처리
+    // 다이얼로그 출력 함수
     private fun showDialogs(title:String, msg:String, dismissListener: Unit?){
         val builder = AlertDialog.Builder(this)
         builder.setTitle(title)
@@ -209,7 +170,7 @@ class MainActivity : AppCompatActivity() {
         builder.setPositiveButton(android.R.string.ok, null)
         builder.setOnDismissListener{dismissListener}
         builder.show()
-    }*/
+    }
 
     private fun saveFcmToken(){
         // fcm 토큰 값 받아오기 나중에 서버로 전송하는 방식으로 교체할 예정
@@ -240,14 +201,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    companion object {
-        const val TAG = "MainActivity"
-        const val PERMISSION_REQUEST_BACKGROUND_LOCATION = 0
-        const val PERMISSION_REQUEST_FINE_LOCATION = 1
-    }
     override fun onDestroy() {
         // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 한다.
         mBinding = null
         super.onDestroy()
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
+        const val PERMISSION_REQUEST_BACKGROUND_LOCATION = 0
+        const val PERMISSION_REQUEST_FINE_LOCATION = 1
     }
 }
