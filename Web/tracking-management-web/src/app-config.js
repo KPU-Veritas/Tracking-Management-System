cc9aea914d29333146c5329e94ac9a00ee4f155c
JungHoiMin <wony1102@naver.com>
2022-09-06 23:03:54 +0900

    웹, 서버 재배포

--- Web/tracking-management-web/src/app-config.js
+++ Web/tracking-management-web/src/app-config.js
@@ -3,14 +3,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
- 
-  //backendHost = "http://localhost:8080"
-  // backendHost = "http://211.245.10.65:8080"
-  backendHost = "http://TMS-SERVER.us-west-2.elasticbeanstalk.com"
-}
-else {
-  //backendHost = "http://211.245.10.65:8080"
-  backendHost = "http://TMS-SERVER.us-west-2.elasticbeanstalk.com"
+  backendHost = "http://TMS-SERVER.ap-northeast-2.elasticbeanstalk.com"
 }
 
 export const API_BASE_URL = `${backendHost}`;
