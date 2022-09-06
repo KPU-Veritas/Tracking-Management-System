b1bd1247eec2456685be7f4e6ab2244a811773a2
JungHoiMin <wony1102@naver.com>
2022-09-06 23:33:21 +0900

    web build 파일 수정

--- Web/tracking-management-web/src/app-config.js
+++ Web/tracking-management-web/src/app-config.js
@@ -4,6 +4,8 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   backendHost = "http://TMS-SERVER.ap-northeast-2.elasticbeanstalk.com"
+}else{
+  backendHost = "http://TMS-SERVER.ap-northeast-2.elasticbeanstalk.com"
 }
 
 export const API_BASE_URL = `${backendHost}`;
