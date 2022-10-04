3112110f617c79d8f5273d42309981dcae7e43e5
ZennoRyun <hklee4578@naver.com>
2022-10-04 12:23:55 +0900

    배포 중단 -> localhost 환경으로 전환

--- Web/tracking-management-web/src/app-config.js
+++ Web/tracking-management-web/src/app-config.js
@@ -2,7 +2,7 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-backendHost = "http://TMS-SERVER.ap-northeast-2.elasticbeanstalk.com"
+backendHost = "http://localhost:8080"
 
 
 export const API_BASE_URL = `${backendHost}`;
